e44be3328d3ed545828174eb7739fae7fc72ca07
matveimug <madis.vaher@gmail.com>
2019-01-28 22:18:17 +0200

    4

--- sketches/4.js
+++ sketches/4.js
@@ -10,11 +10,14 @@ window.onresize = function() {
 let rotation = 0;
 let rotation2 = 0;
 let speed = 10;
-let levels = 15;
+let levels = 6;
 let count = 0;
 
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
 function setup() {
-    background('white');
     createCanvas(screenW, screenH);
     angleMode(DEGREES);
     // noLoop();
@@ -22,28 +25,26 @@ function setup() {
 }
 
 function draw() {
+
     // scale();
     translate(screenW / 2, screenH / 2);
     rotate(rotation2);
-    // clear();
     rotation2 = rotation2 - speed/50;
-    drawCircle(screenW / 2, 150, levels);
+    // background('rgba(0,0,0,0.01)');
+    drawCircle(screenW / 2, (screenH+screenW)/30, levels);
 }
 
 function drawCircle(x, radius, level) {
     count++;
     rotation = rotation + speed/100;
     rotate(rotation);
-    translate(radius/2, 0);
-    if (level % 2 == 0) {
-        fill('yellow');
-    } else {
-        fill('white');
+    if (level !== levels) {
+        translate(radius * scale((mouseX+mouseY),0,(screenW+screenH),1,6), 0);
+        ellipse(0, 0, radius , radius);
     }
-    ellipse(0, 0, radius , radius);
     // text(level, 0, 0);
     if (level > 0) {
         level = level - 1;
-        drawCircle(x, radius / 1.2, level);
+        drawCircle(x, radius / 1.5, level);
     }
 }
